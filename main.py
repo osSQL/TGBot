32bf1872985e755e28826dc32b29ecaccee5f7e8
kirillpedev <kirillpetrov2511@gmail.com>
2024-05-30 12:58:26 +0300

    second

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard= True)
+button_1 = KeyboardButton('Фото неба')
+button_2 = KeyboardButton('След')
+keyboard.add(button_1,button_2)
+
+keyboard2 = ReplyKeyboardMarkup(resize_keyboard= True)
+button_3 = KeyboardButton('Фото')
+button_4 = KeyboardButton('Назад')
+keyboard2.add(button_3,button_4)
 
 async def set_commands(bot: Bot):
     commands = [
@@ -13,8 +24,22 @@ async def set_commands(bot: Bot):
 
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.reply('Я ЭХО бот')
+    await message.reply('Я ЭХО бот', reply_markup=keyboard)
 
+@dp.message_handler(lambda message: message.text == "Фото неба")
+async def button_1_click(message: types.message):
+    await bot.send_photo(message.chat.id, photo='https://www.google.com/imgres?q=berserk%20sky&imgurl=https%3A%2F%2Fexternal-preview.redd.it%2FmZDbGvIdZEInyKExlFMEpCG0perWu4EXXSt5UCdcgzI.jpg%3Fauto%3Dwebp%26s%3D80b05a2adbd4e1b705039984c457bc55ef6317cc&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2FBerserk%2Fcomments%2F90u8pg%2Fgazing_up_at_the_dark_sky%2F&docid=bX2CCvJtzzHTKM&tbnid=gDxDv5HiO6FowM&vet=12ahUKEwjy062agLWGAxWbCBAIHUWlAbQQM3oECBsQAA..i&w=600&h=315&hcb=2&ved=2ahUKEwjy062agLWGAxWbCBAIHUWlAbQQM3oECBsQAA',caption="Небо")
+@dp.message_handler(lambda message: message.text == "След")
+async def button_2_click(message: types.message):
+    await message.answer('2/2',reply_markup=keyboard2)
+
+@dp.message_handler(lambda message: message.text == "Фото")
+async def button_3_click(message: types.message):
+    await bot.send_photo(message.chat.id, photo= 'https://cdn.britannica.com/95/156695-131-FF89C9FA/oak-tree.jpg?w=200&h=200&c=crop')
+
+@dp.message_handler(lambda message: message.text == "Назад")
+async def button_4_click(message: types.message):
+    await message.answer("1/2", reply_markup= keyboard)
 @dp.message_handler(commands= 'help')
 async def help(message: types.Message):
     await message.reply('Я готов помочь с...')
