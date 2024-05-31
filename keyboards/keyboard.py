9d9c0ed502c4757aff7610335c35b38b402f15e2
kirillpedev <kirillpetrov2511@gmail.com>
2024-05-31 09:01:11 +0300

    three(fixed)

---
+++ keyboards/keyboard.py
@@ -0,0 +1,15 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+
+def get_keyboard_1():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard= True)
+    button_1 = KeyboardButton('Фото неба')
+    button_2 = KeyboardButton('След')
+    keyboard.add(button_1,button_2)
+    return keyboard
+
+def get_keyboard_2():
+    keyboard2 = ReplyKeyboardMarkup(resize_keyboard= True)
+    button_3 = KeyboardButton('Фото')
+    button_4 = KeyboardButton('Назад')
+    keyboard2.add(button_3,button_4)
+    return keyboard2
